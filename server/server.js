37aac0106d01a3b0ed34fa65e2b1b199e1e337ed
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-11 15:22:18 -0700

    committing code for technext-chart

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const compression = require('compression')
+const axios = require('axios')
+const app = express()
+app.use(compression())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    axios.get('https://testtechnext1-pearl118.b4a.run/search/api/phases/')
+    .then(response => {
+        res.json(response.data)
+        console.log(response.data)
+    })
+    .catch(error => {
+        res.status(500).json({error: 'An error occured'})
+        console.log(error)
+    })
+})
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000')
+})
---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, Tooltip, XAxis, YAxis } from 'recharts'
+import axios from 'axios'
+
+function App() {
+
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(true)
+
+
+
+  useEffect(() => {
+    axios.get('http://localhost:5000')
+      .then((res) => {
+        setData(res.data)
+        setLoading(false)
+      })
+  }, [])
+
+  console.log(data)
+
+
+  return (
+    <>
+      {
+        loading
+          ?
+          <h2>Loading data...</h2>
+          :
+          <div>
+            <h1>TechNext Bar/Line Chart</h1>
+            <ComposedChart width={400} height={300} data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="phase" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="entries" fill="#8884d8" />
+              <Line type="monotone" dataKey="entries" stroke="black" />
+            </ComposedChart>
+          </div>
+
+      }
+
+    </>
+  )
+}
+
+export default App
